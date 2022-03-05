4f58dfcb79cdba4e4e47bded2e385d74e3cb75c7
izereuwonkunda <izereuwonkundakelia@gmail.com>
2022-03-05 22:14:20 +0200

    MVC

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const authRoutes=require('./routes/authRoutes')
 const app = express();
 
 // middleware
 app.use(express.static('public'));
-
+app.use(express.json());
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = 'mongodb://localhost/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
@@ -18,3 +18,4 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes)
